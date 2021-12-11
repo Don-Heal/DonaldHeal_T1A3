11dd4465d02b4651e0f595c2ebb5e3ed46a8e202
don-heal <donnie.heal@gmail.com>
2021-12-11 23:09:27 +1100

    changed gem file reworked multiplayer turn taking and started ship deployment to the board yet to get ships to board.

--- src/main.rb
+++ src/main.rb
@@ -1,15 +1,21 @@
 require_relative "./methods.rb"
-require_relative "./classes/ships.rb"
-require_relative "./classes/grid.rb"
 
 puts title
+ship1 = ships[0]
+ship2 = ships[1]
+ship3 = ships[2]
 
-if gamemode == 1                                                 # find out if they want to play againt the computer or another player
+ship1_hits = []
+ship2_hits = []
+ship3_hits = []
+
+# find out if they want to play againt the computer or another player
+
+if gamemode == 1
   puts "Please enter Username".colorize(:blue)
   input1 = gets.chomp.to_s
   player1 = input1
-
-else                                                             # get both players name
+else # get both players name
   puts "Please enter player 1's Username name".colorize(:blue)
   input1 = gets.chomp.to_s
   player1 = input1
@@ -18,14 +24,62 @@ else                                                             # get both play
   player2 = input2
 end
 
-system "clear"
-                                                                # Start the game with player 1
-puts title
-puts "It's #{player1} turn".colorize(:blue)
-if players_turn == 1
-    puts players_grid
-elsif players_turn == 2
-    # calls shoot method
-else
-    # are you sure you would like to exit?
+if player2 == nil
+  puts diffuculty
+end
 
+system "clear"
+
+# Start the game with player 1
+
+puts title
+
+# Players options for their turn
+# player 1's turn
+while true
+  while player2 = player2
+    puts "it's #{player1}'s turn".colorize(:blue)
+    case players_turn
+    when 1
+      puts "This is #{player1}'s side of the field".colorize(:blue)
+      puts board
+      puts "This is the enemy's side of the field".colorize(:red)
+      puts board
+    when 2
+      puts "Please enter where you would like to shoot A1 is top left and G1 is bottom left"
+
+      break
+    when 3
+      exit
+    end
+  end
+  # player 2's turn
+  while player2 = player2
+    puts "it's #{player2}'s turn".colorize(:red)
+    case players_turn
+    when 1
+      puts "This is #{player2}'s side of the field".colorize(:red)
+      puts board
+      puts "This is the enemy's side of the field".colorize(:blue)
+      puts board
+    when 2
+      break
+    when 3
+      exit
+    end
+  end
+  # single player option
+  while player2 == nil
+    puts "it's #{player1}'s turn".colorize(:blue)
+    case players_turn
+    when 1
+      puts "This is #{player1}'s side of the field".colorize(:blue)
+      puts board
+      puts "This is the enemy's side of the field".colorize(:red)
+      puts board
+    when 2
+    when 3
+      exit
+    end
+  end
+end
--- src/methods.rb
+++ src/methods.rb
@@ -2,49 +2,83 @@ require "tty-prompt"
 require "colorize"
 require "artii"
 
+# Displayes title screen
 def title
   a = Artii::Base.new
   a.asciify("Battleships")
   puts a.asciify("B A T T L E S H I P S")
 end
 
+# selects gamemode when prompted
 def gamemode
   choice = { Singleplayer: 1, Multiplayer: 2 }
   prompt = TTY::Prompt.new
   prompt.select("which mode would you like to play?", choice)
 end
 
+# sets Computers shot to random or seeking once hit
+def diffuculty
+  choice = { Easy: 1, Hard: 2 }
+  prompt = TTY::Prompt.new
+  prompt.select("What skill level would you like to play?", choice)
+end
+
+# gives players options to select from
 def players_turn
   choice = { Checkboard: 1, Shoot: 2, Surrender: 3 }
   prompt = TTY::Prompt.new
   prompt.select("What would you like to do?", choice)
 end
 
-def grid
-  puts "---------------"
-
-  a = ["+" " " "+" " " "+" " " "+" " " "+" " " "+" " " "+" " " "+"]
-  b = ["+" " " "+" " " "+" " " "+" " " "+" " " "+" " " "+" " " "+"]
-  c = ["+" " " "+" " " "+" " " "+" " " "+" " " "+" " " "+" " " "+"]
-  d = ["+" " " "+" " " "+" " " "+" " " "+" " " "+" " " "+" " " "+"]
-  e = ["+" " " "+" " " "+" " " "+" " " "+" " " "+" " " "+" " " "+"]
-  f = ["+" " " "+" " " "+" " " "+" " " "+" " " "+" " " "+" " " "+"]
-  g = ["+" " " "+" " " "+" " " "+" " " "+" " " "+" " " "+" " " "+"]
-  h = ["+" " " "+" " " "+" " " "+" " " "+" " " "+" " " "+" " " "+"]
-  puts a
-  puts b
-  puts c
-  puts d
-  puts e
-  puts f
-  puts g
-  puts h
-  puts "---------------"
+def board
+  @x = ["A", "B", "C", "D", "E", "F", "G", "H"]
+  @y = [1, 2, 3, 4, 5, 6, 7, 8]
+  @xy = @x.product(@y)
+  @board = @xy.map! { |xy| xy.join("") }
+  puts @board[0..7].join(" ")
+  puts @board[8..15].join(" ")
+  puts @board[16..23].join(" ")
+  puts @board[24..31].join(" ")
+  puts @board[32..39].join(" ")
+  puts @board[40..47].join(" ")
+  puts @board[48..55].join(" ")
+  puts @board[56..63].join(" ")
 end
 
-def players_grid
-    puts grid
+def ships
+  ships_array = []
+
+  ships_array << ["A1", "B1", "C1", "D1"]
+  ships_array << ["H8", "G8", "F8", "E8"]
+  ships_array << ["C3", "C4", "C5", "C6"]
+  ships_array << ["A2", "A3", "A4"]
+  ships_array << ["B4", "B5", "B6"]
+  ships_array << ["H4", "H5", "H6"]
+  ships_array << ["G1", "G2", "G3"]
+  ships_array << ["A5", "A6", "A7"]
+  ships_array << ["G4", "G5", "G6"]
+  ships_array << ["B8", "C8", "D8"]
+  ships_array << ["C7", "D7", "E7"]
+  ships_array << ["F1", "F2", "F3"]
+
+  ship_1 = ships_array.sample(1)
+  ship_2 = ships_array.sample(1)
+  ship_3 = ships_array.sample(1)
+
+  while ship_1 == ship_2
+    ship_1.replace(ships_array.sample(1))
+  end
+  while ship_2 == ship_3
+    ship_2.replace(ships_array.sample(1))
+  end
+  while ship_3 == ship_1
+    ship_3.replace(ships_array.sample(1))
+  end
+  return [ship_1, ship_2, ship_3]
 end
 
-def enemy_grid
+def sunk
+  a = Artii::Base.new
+  a.asciify("Battleships")
+  puts a.asciify("You Sunk a Ship".colorize(:green))
 end
